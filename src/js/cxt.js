eb0193761efbdc11c57043074f1fbe63093f7f3f
MoXuanHeng <2382252568@qq.com>
2019-07-20 14:20:52 +0800

    版本1.2.0

--- src/js/cxt.js
+++ src/js/cxt.js
@@ -1,6 +1,6 @@
 // console.log("超星通注入成功")
 
-//$("#iframe").contents().find("iframe").contents().find("video")
+//jQuery("#iframe").contents().find("iframe").contents().find("video")
 
 (function cxtFn(setConfig){
    // 运行脚本判断
@@ -20,17 +20,21 @@
         autodt   = setConfig.autodt,   // 自动答题
         autonext = setConfig.autonext, // 自动下一章
         state    = setConfig.state;   // 播放状态
-    window.alert=none;
-    var iframVideo = $("#iframe").contents().find("iframe").contents();
+	// 关闭弹窗
+	window.alert=null;
+    var iframVideo = jQuery("#iframe").contents().find("iframe").contents();
     // 移除监听（可以倍数可以一步到最后）
     iframVideo.find(".x-body").unbind();
+	// 切换低画质
+	iframVideo.find(".vjs-resolution-button-label").trigger("click");
+	console.log("切换画质OK")
     // 播放
     v = iframVideo.find("video");
     setTimeout(function(){
       // 监听事件
       if(autoplay=="true"&&v[0].paused)
       v[0].play();
-      console.log("正在播放:"+$("#iframe").contents().find("p .ans-attach-ct .ans-job-icon").text());
+      console.log("正在播放:"+jQuery("#iframe").contents().find("p .ans-attach-ct .ans-job-icon").text());
     },1000);
     // 静音
     if(muted=="false") v[0].muted=false;
@@ -41,13 +45,13 @@
       setTimeout(function () {
        if(v[0].playbackRate!=speed)
         v[0].playbackRate=speed;
-        // $(".speedBox").eq(0).trigger("click");
+        // jQuery(".speedBox").eq(0).trigger("click");
         console.log("已经开启"+speed+"倍速！");
       },2000);
     // 答题
     if(autodt=="true"&&videoDot==undefined){
       // 先判断有没有题目
-      videoDot = $("#iframe").contents().find("iframe").contents().find(".x-container");
+      videoDot = jQuery("#iframe").contents().find("iframe").contents().find(".x-container");
       timer=setInterval(function(){
         if(autoplay=="true"&&v[0].paused)
         v[0].play();
@@ -76,9 +80,10 @@
        // 定时器关闭
        if(timer) clearInterval(timer);
       // 视频结束
-      var menuList = $("#coursetree .ncells");
-      var nextindex =menuList.find("h4.currents").removeClass("currents").parent().parent().index();
-      menuList.eq(nextindex).find("h4").addClass("currents").trigger("click");
+      var menuList = jQuery("#coursetree .ncells");
+      var nextindex =jQuery("#coursetree .ncells h4.currents").removeClass("currents").parent().index();
+      menuList.eq(nextindex).find("h4").addClass("currents");
+	  menuList.eq(nextindex).find("span").trigger("click");
       // 下一章
         console.log("正在播放下一章！....");
         setTimeout(function(){
@@ -97,7 +102,7 @@
           if(setConfig.state=="true") sendResponse("恭喜你,最爱助手正在运行中....");
           else{
             sendResponse("恭喜你,最爱助手停止成功....");
-            $("#iframe").contents().find("iframe").contents().find("video")[0].playbackRate=1;
+            jQuery("#iframe").contents().find("iframe").contents().find("video")[0].playbackRate=1;
             if(timer) clearInterval(timer);
           } 
           cxtFn(setConfig); //执行数据
